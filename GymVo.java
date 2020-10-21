e3bf601bb21ed0671285592cecd3825affefa9a1
de526 <de526@daum.net>
2020-10-21 15:24:35 +0900

    vovo

---
+++ GymVo.java
@@ -0,0 +1,60 @@
+package com.jasla.ConnectGym.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class GymVo {
+	private int gNo;
+	private String gName;
+	private String gInfo;
+	private String gNum;
+	private String gAddr;
+	private String gX;
+	private String gY;
+	
+	
+	public int getgNo() {
+		return gNo;
+	}
+	public void setgNo(int gNo) {
+		this.gNo = gNo;
+	}
+	public String getgName() {
+		return gName;
+	}
+	public void setgName(String gName) {
+		this.gName = gName;
+	}
+	public String getgInfo() {
+		return gInfo;
+	}
+	public void setgInfo(String gInfo) {
+		this.gInfo = gInfo;
+	}
+	public String getgNum() {
+		return gNum;
+	}
+	public void setgNum(String gNum) {
+		this.gNum = gNum;
+	}
+	public String getgAddr() {
+		return gAddr;
+	}
+	public void setgAddr(String gAddr) {
+		this.gAddr = gAddr;
+	}
+	public String getgX() {
+		return gX;
+	}
+	public void setgX(String gX) {
+		this.gX = gX;
+	}
+	public String getgY() {
+		return gY;
+	}
+	public void setgY(String gY) {
+		this.gY = gY;
+	}
+	
+}
